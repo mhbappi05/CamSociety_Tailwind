514f90bb565343630cb478abfb3e5c4d0bcb59ff
mhbappi05 <mhbappi05@gmail.com>
2025-01-08 23:17:34 +0600

    #2nd commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", 
+    "./src/**/*.{js,ts,jsx,tsx}", 
+    "./components/**/*.{vue,js}", 
+    "./pages/**/*.{html,js}" 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
